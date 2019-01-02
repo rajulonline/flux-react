ba5ac1bd7afa2c64f7f008a68fe053e17a19a8dd
Lakshmi Narasimhan <rajulonline@gmail.com>
2019-01-01 23:38:35 -0800

    simple flux pattern with react to fetch data from git by username

--- app/javascript/actions/GitRetrieveDetailsActions.jsx
+++ app/javascript/actions/GitRetrieveDetailsActions.jsx
@@ -1,8 +1,8 @@
 import API from "../API.jsx";
 
 export default {
-	getAllUserDetails() {
-		console.log(1,"Here you are at the actions");
-		API.getAllUserDetails();
+	getAllUserDetails(userName) {
+		console.log(userName,"Here you are at the actions");
+		API.getAllUserDetails(userName);
 	}
 }
--- app/javascript/stores/GetUserDetailsStore.jsx
+++ app/javascript/stores/GetUserDetailsStore.jsx
@@ -1,12 +1,42 @@
 import AppDispatcher from "../dispatcher.jsx";
 import ActionTypes from "../constants.jsx";
+import {EventEmitter} from "events";
+const CHANGE_EVENT = "CHANGE";
+
+var gitUserDetails = [];
+
+class GitEventEmitter extends EventEmitter {
+	getAll() {
+		return gitUserDetails;
+	}
+
+	emitChange() {	
+	console.log("Emit change called");	
+		this.emit(CHANGE_EVENT);
+	}
+
+	addChangeListener(callback) {
+		this.on(CHANGE_EVENT, callback);
+	}
+
+	removeChangeListener(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	}
+}
+
+let GetUserDetailsStore = new GitEventEmitter();
 
 AppDispatcher.register(action => {
-	console.log(4, "At the Stores");
+	
 	switch(action.actionType) {
-		case ActionTypes.RECEIVED_GET_USER_DETAILS:
-
+		case ActionTypes.RECEIVED_GET_USER_DETAILS:		
+			gitUserDetails.push(action.userDetails);
+			console.log(gitUserDetails, "At the Stores");			
+			GetUserDetailsStore.emitChange();
 		break;
 		default:
+
 	}
 });
+
+export default GetUserDetailsStore;
