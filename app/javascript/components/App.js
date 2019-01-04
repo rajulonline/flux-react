62667a061da8b76b129125873ada17a13b0e07db
rajulonline <rajulonline@gmail.com>
2019-01-03 18:36:25 -0800

    resolve merge conflicts

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,64 +4,42 @@ import Form from './Form.js';
 import UserList from './UserList.js';
 import GetUserDetailsStore from "../stores/GetUserDetailsStore.jsx";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
 let getAppState = () => {  
   return { userDetails: GetUserDetailsStore.getAll() };
 }
 
 class App extends React.Component {
-<<<<<<< HEAD
+
   // constructor(props){
   //   super(props);
   state = getAppState();
   //_onChange = this._onChange.bind(this);
-  
-=======
-  constructor(props){
-    super(props);
-    this.state = getAppState();
-    this._onChange = this._onChange.bind(this);
-  }
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
-  
-  
+ 
 	// addUserDetails = (userDetailsFromResponse) => {
 	// 	this.setState({userDetails: this.state.userDetails.concat(userDetailsFromResponse)})
 	// }
 
-<<<<<<< HEAD
+
   componentDidMount = () => {    
-=======
-  componentDidMount() {    
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
     console.log("component did mount called ");
     GetUserDetailsStore.addChangeListener(this._onChange);
   }
 
-<<<<<<< HEAD
+
   componentWillUnmount = () => {
-=======
-  componentWillUnmount() {
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
      console.log("component will un mount called ");
     GetUserDetailsStore.removeChangeListener(this._onChange);
   }
 
-<<<<<<< HEAD
+
   _onChange = () => {  
-=======
-  _onChange() {  
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
   console.log("On change called");   
     this.setState(getAppState());
   }
 
   render () {
     return (
-<<<<<<< HEAD
+
       <div>  
                 <div>
         <p align="center">
@@ -71,12 +49,7 @@ class App extends React.Component {
         </p>    
       </div>
         <Form />      
-        <UserList allDetails={this.state.userDetails}/>                  
-=======
-      <div>       
-        <Form />
-        <UserList allDetails= {this.state.userDetails} />
->>>>>>> 26b013e6f66171190879cf730c2b688d494fd929
+   
       </div>
     );
   }
